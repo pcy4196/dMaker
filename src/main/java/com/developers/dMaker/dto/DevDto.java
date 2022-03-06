e22388d0c4553f9758d0e9be735768739fbab8d0
PCY <pcy4196@naver.com>
2022-03-07 00:13:10 +0900

    2022.03.07 -- commit -- Ch01. 롬북 관련 교육 작성

--- src/main/java/com/developers/dMaker/dto/DevDto.java
+++ src/main/java/com/developers/dMaker/dto/DevDto.java
@@ -1,6 +1,7 @@
 package com.developers.dMaker.dto;
 
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
@@ -10,9 +11,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor          // 기본생성자
 @AllArgsConstructor         // 모든 파라미터 생성자
 @RequiredArgsConstructor    // 필수 파라미터(@NonNull) 생성자
+@Builder
+@Slf4j
 public class DevDto {
     @NonNull
     String firstName;
     Integer age;
     LocalDateTime startAt;
+
+    public void printLog() {
+        log.info(getFirstName());
+    }
 }
--- src/test/java/com/developers/dMaker/dto/DevDtoTest.java
+++ src/test/java/com/developers/dMaker/dto/DevDtoTest.java
@@ -17,5 +17,17 @@ class DevDtoTest {
         devDto.setStartAt(LocalDateTime.now());
         System.out.println(devDto);
         // DevDto(firstName=snow, age=22, startAt=2022-03-06T23:50:13.984636)
+
+        // @Builder
+        DevDto devDto1 = DevDto.builder()
+                .firstName("snow1")
+                .age(22)
+                .startAt(LocalDateTime.now())
+                .build();
+
+        System.out.println(devDto1);
+        // DevDto(firstName=snow1, age=22, startAt=2022-03-07T00:00:30.085155)
+        devDto1.printLog();
+        // 00:06:38.730 [main] INFO com.developers.dMaker.dto.DevDto - snow1
     }
 }
